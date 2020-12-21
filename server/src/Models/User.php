2d795a86b6de44951937a9009ddf20482558ad7d
Ivaylo Banchevski <ivaylo.banchevski@gmail.com>
2020-12-22 00:52:18 +0200

    Add list all users and get user by id
    A few static methods (get all, get by id) are added to the user model. They make use of the database connector singleton
    and are somewhat examplatory of how the model logic (in my opinion) should be written.

--- server/src/Models/User.php
+++ server/src/Models/User.php
@@ -1,6 +1,8 @@
 <?php
 namespace Src\Models;
 
+use Src\Utils\DBConnector;
+
 class User {
     private $dbConn = null;
 
@@ -12,5 +14,19 @@ class User {
         $query = $this->dbConn->prepare("select * from patients");
         $query->execute();
         return $query->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public static function all() {
+        $db = DBConnector::getInstance()->getConnection();
+        $query = $db->prepare("select id,email,name from patients");
+        $query->execute();
+        return $query->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public static function getById($id) {
+        $conn = DBConnector::getInstance()->getConnection();
+        $query = $conn->prepare('select id,name,email from patients where id=:id');
+        $query->execute(["id" => $id]);
+        return $query->fetch(\PDO::FETCH_ASSOC);
     }
 }
